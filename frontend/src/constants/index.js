9e00c1dc0419b3aa9c749301a0e4b322ed05dace
StevenMadali <stevenmadali17@gmail.com>
2024-10-07 20:19:04 +0800

    Add Navbar on Frontend

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,9 @@
+
+// add more in mapping for more navlinks
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#products", label: "Products" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#contact-us", label: "Contact Us" },
+];
